464c43f45a03599f5951f0ad1a5dd6eba833cdd4
Juan José Vázquez Gil <juanjose.vazquez@prefapp.es>
2025-07-07 15:55:35 +0200

    fix: Container plugin inputs

--- gh/main.go
+++ gh/main.go
@@ -70,9 +70,13 @@ func (m *Gh) Container(
 	// +optional
 	repo string,
 
-	// Gh plugins
+	// Gh plugin names
 	// +optional
-	plugins []GHPlugin,
+	pluginNames []string,
+
+	// Gh plugin names
+	// +optional
+	pluginVersions []string,
 
 ) (*dagger.Container, error) {
 	file, err := lo.Ternary(version != "", m.Binary.WithVersion(version), m.Binary).binary(ctx)
@@ -86,7 +90,7 @@ func (m *Gh) Container(
 	// update the container with the given token and repository if provided
 	gc = lo.Ternary(token != nil, gc.WithToken(token), gc)
 	gc = lo.Ternary(repo != "", gc.WithRepo(repo), gc)
-	gc = lo.Ternary(plugins != nil, gc.WithPlugins(plugins), gc)
+	gc = lo.Ternary(plugins != nil, gc.WithPlugins(pluginNames, pluginVersions), gc)
 
 	// get the container object with the given binary
 	ctr := gc.container(file)
